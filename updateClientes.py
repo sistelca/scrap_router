ed1decee65f8195fdc647365d57799cb3c4611c0
luis castillo <sistelca2013@gmail.com>
2021-04-13 06:07:44 -0400

    act

--- updateClientes.py
+++ updateClientes.py
@@ -3,6 +3,8 @@
 import mysql.connector
 from dotenv import load_dotenv
 import os
+import datetime
+import json
 
 load_dotenv()
 
@@ -23,16 +25,34 @@ def conexion(query):
         cursor = cnx.cursor()
         seleccion = (query)
         cursor.execute(seleccion)
-        return list(cursor)
+        return (list(cursor), cursor.description)
     
 
 querys = ['update Actzl set pasar = 1 where pasar = 2',
           'select * from Actzl where pasar = 1',
           'update Actzl set pasar = 0 where pasar = 1']
-    
-for query in querys:
-    ok = conexion(query)
-    print(ok)
-    
 
-print("xd")
+claves = ''
+    
+okis = []
+for query in querys:
+    okis.append(conexion(query))
+    
+field_names = [i[0] for i in okis[1][1]]
+
+print(field_names)
+
+pila = []
+for ok in okis[1][0]:
+    uni = {}
+    for l, v in zip(field_names[:-1], ok[:-1]):
+        if isinstance(v, datetime.datetime):
+            uni[l] = str(v)
+        else:
+            uni[l] = v
+    pila.append(uni)
+    #break
+
+with open('result.json', 'w') as fp:
+    json.dump(pila, fp)
+print('listo')
